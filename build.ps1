3b059858499104c2baacbae886931182e2b366dc
guanghechen <me.guanghechen@gmail.com>
2024-05-20 14:31:40 +0800

    improve: update nvim-oxi

---
+++ build.ps1
@@ -0,0 +1,13 @@
+# Navigate to the Spectre-oxi directory
+cd "spectre-oxi"
+
+# Assuming cargo build is run from the root of the project, navigate there if necessary
+# Navigate to the project directory where Cargo.toml is located
+
+# Build your Rust project using Cargo release build
+cargo build --release
+
+# Copy the file `a.txt` from target/release/ to lua/ in your current directory
+Copy-Item -Path target/release/spectre-oxi.dll -Destination ../lua/
+Copy-Item -Path target/release/spectre-oxi.dll.lib -Destination ../lua/
+
