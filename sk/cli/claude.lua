4a492da6e863d06b15e887be97dd43c50b76f11d
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-17 07:50:54 +0200

    feat(cli.claude): transform line ranges to a format Claude can understand. Closes #59

--- sk/cli/claude.lua
+++ sk/cli/claude.lua
@@ -4,8 +4,17 @@ return {
   is_proc = "\\<claude\\>",
   url = "https://github.com/anthropics/claude-code",
   format = function(text)
-    require("sidekick.text").transform(text, function(str)
+    local Text = require("sidekick.text")
+
+    Text.transform(text, function(str)
       return str:find("[^%w/_%.%-]") and ('"' .. str .. '"') or str
     end, "SidekickLocFile")
+
+    local ret = Text.to_string(text)
+
+    -- transform line ranges to a format that Claude understands
+    ret = ret:gsub("@([^@]-) :L(%d+)%-L(%d+)", "@%1#L%2-%3")
+
+    return ret
   end,
 }
