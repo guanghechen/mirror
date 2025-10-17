19b59854782837430ae297ed4690660b7aa254d5
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-17 07:41:07 +0200

    feat(cli): handle special filenames for Claude, Gemini, Qwen. Fixes #130

--- sk/cli/claude.lua
+++ sk/cli/claude.lua
@@ -2,5 +2,10 @@
 return {
   cmd = { "claude" },
   is_proc = "\\<claude\\>",
-  url = "https://github.com/anthropics/claude-code"
+  url = "https://github.com/anthropics/claude-code",
+  format = function(text)
+    require("sidekick.text").transform(text, function(str)
+      return str:find("[^%w/_%.%-]") and ('"' .. str .. '"') or str
+    end, "SidekickLocFile")
+  end,
 }
--- sk/cli/gemini.lua
+++ sk/cli/gemini.lua
@@ -2,5 +2,10 @@
 return {
   cmd = { "gemini" },
   is_proc = "\\<gemini\\>",
-  url = "https://github.com/google-gemini/gemini-cli"
+  url = "https://github.com/google-gemini/gemini-cli",
+  format = function(text)
+    require("sidekick.text").transform(text, function(str)
+      return str:gsub("([^%w/_%.%-])", "\\%1")
+    end, "SidekickLocFile")
+  end,
 }
