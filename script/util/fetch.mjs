0ff2628344d38e13c354273f5c7d3b4d2bb91872
guanghechen <me.guanghechen@gmail.com>
2025-12-27 16:01:18 +0800

    :bug: fix(fetch): use branch reset instead of delete-recreate for worktree compatibility

--- script/util/fetch.mjs
+++ script/util/fetch.mjs
@@ -8,34 +8,30 @@ import { run_command } from './command.mjs'
 export async function fetch_repo(localBranchName, item) {
   const remote = `${item.remote}.git`
   const remoteBranchName = item.branch
-  const originName = `origin_${  localBranchName}`
+  const originName = `origin_${localBranchName}`
 
   const cmds = {
     add_remote: `git remote add ${originName} ${remote}`,
     fetch_remote: `git fetch ${originName} ${remoteBranchName}`,
-    del_branch: `git branch -D ${localBranchName}`,
-    add_branch: `git branch ${localBranchName} ${originName}/${remoteBranchName}`,
+    reset_branch: `git branch -f ${localBranchName} ${originName}/${remoteBranchName}`,
   }
 
   await run_command(cmds.add_remote, true, true, false)
   await run_command(cmds.fetch_remote, true, true, true)
-  await run_command(cmds.del_branch, true, true, false)
-  await run_command(cmds.add_branch, true, true, true)
+  await run_command(cmds.reset_branch, true, true, true)
 }
 
 export async function fetch_repo_pinned(localBranchName, item) {
   const remote = `${item.remote}.git`
-  const originName = `origin_${  localBranchName}`
+  const originName = `origin_${localBranchName}`
 
   const cmds = {
     add_remote: `git remote add ${originName} ${remote}`,
     fetch_remote: `git fetch ${originName} ${item.commit}`,
-    del_branch: `git branch -D ${localBranchName}`,
-    add_branch: `git branch ${localBranchName} ${item.commit}`,
+    reset_branch: `git branch -f ${localBranchName} ${item.commit}`,
   }
 
   await run_command(cmds.add_remote, true, true, false)
   await run_command(cmds.fetch_remote, true, true, true)
-  await run_command(cmds.del_branch, true, true, false)
-  await run_command(cmds.add_branch, true, true, true)
+  await run_command(cmds.reset_branch, true, true, true)
 }
