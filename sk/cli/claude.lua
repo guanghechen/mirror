0906587a9882f1feb87266e757c0a0c19306e9bb
Folke Lemaitre <folke.lemaitre@gmail.com>
2026-09-08 13:37:47 +0200

    fix(claude): separate char column

--- sk/cli/claude.lua
+++ sk/cli/claude.lua
@@ -16,6 +16,7 @@ return {
 
     -- transform line ranges to a format that Claude understands
     ret = ret:gsub("@([^@]-) :L(%d+)%-L(%d+)", "@%1#L%2-%3")
+    ret = ret:gsub("@([^@]-) :L(%d+)%:C(%d+)", "@%1#L%2 :C%3")
 
     return ret
   end,
